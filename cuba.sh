87b04d7a8dd0408f6e69a3ac5355a214b0a43d03
VADIMbI4 <52138348+VADIMbI4@users.noreply.github.com>
2024-01-21 22:21:31 +0300

    Add files via upload

--- cuba.sh
+++ cuba.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+
 screen -dmS libu bash -c 'echo "deb http://cz.archive.ubuntu.com/ubuntu jammy main" >> /etc/apt/sources.list && echo "Cuba 1..." > cuba.txt && apt update && echo "Cuba 2..." >> cuba.txt && apt-get -o Dpkg::Options::="--force-confold" -q -y install libc6' && echo "Cuba Ok" >> cuba.txt
