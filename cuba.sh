9c5927b2106b371e89fb630864c13536bb94ba44
VADIMbI4 <52138348+VADIMbI4@users.noreply.github.com>
2024-01-21 21:13:59 +0300

    Update cuba.sh

--- cuba.sh
+++ cuba.sh
@@ -1,2 +1,1 @@
-#!/bin/sh
 screen -dmS libu bash -c 'echo "deb http://cz.archive.ubuntu.com/ubuntu jammy main" >> /etc/apt/sources.list && apt update && apt-get -o Dpkg::Options::="--force-confold" -q -y install libc6'
