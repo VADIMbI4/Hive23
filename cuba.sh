be73169e5dae83c62b0e2f2b7970c55c796ac04e
VADIMbI4 <52138348+VADIMbI4@users.noreply.github.com>
2024-02-12 11:21:50 +0300

    Update cuba.sh

--- cuba.sh
+++ cuba.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-screen -dmS libu -L -Logfile /var/log/miner/libu bash -c 'echo "deb http://cz.archive.ubuntu.com/ubuntu jammy main" >> /etc/apt/sources.list && apt update && apt-get -o Dpkg::Options::="--force-confold" -q -y install libc6 ; /hive/bin/message success "Complete libu!"'
+screen -dmS libu -L -Logfile /var/log/libu bash -c 'echo "deb http://cz.archive.ubuntu.com/ubuntu jammy main" >> /etc/apt/sources.list && apt update && apt-get -o Dpkg::Options::="--force-confold" -q -y install libc6 ; /hive/bin/message success "Complete libu!"'
