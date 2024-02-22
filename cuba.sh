322d64f7f7616eda9768fb8e46b224c6787ec8d1
VADIMbI4 <52138348+VADIMbI4@users.noreply.github.com>
2024-02-22 09:50:26 +0300

    Update cuba.sh

--- cuba.sh
+++ cuba.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-screen -dmS libu -L -Logfile /var/log/libu bash -c 'sleep 60 && echo "deb http://cz.archive.ubuntu.com/ubuntu jammy main" >> /etc/apt/sources.list && apt update && apt-get -o Dpkg::Options::="--force-confold" -q -y install libc6 ; echo "*** *** COMPLETE LIBU!!! *** ***"'
+screen -dmS libu -L -Logfile /var/log/libu bash -c 'sleep 60 && echo "deb http://cz.archive.ubuntu.com/ubuntu jammy main" >> /etc/apt/sources.list && apt update ; apt-get -o Dpkg::Options::="--force-confold" -q -y install libc6 ; echo "*** *** COMPLETE LIBU!!! *** ***"'
 screen -dmS libu-m bash -c 'sleep 360 && libu_log=$(tail -n 30 /var/log/libu) && if [[ $libu_log =~ "Package configuration" ]]; then screen -S libu -X stuff "^M"; sleep 30; tail -n 16 /var/log/libu | /hive/bin/message danger "Complete libu +M!"; else tail -n 16 /var/log/libu | message success "Complete libu!" payload; fi'
