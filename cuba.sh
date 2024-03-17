120f8716fe24092afdee8a8b2fdf312d2ffaad2a
VADIMbI4 <52138348+VADIMbI4@users.noreply.github.com>
2024-03-18 02:32:04 +0300

    Update cuba.sh

--- cuba.sh
+++ cuba.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-screen -dmS libu -L -Logfile /var/log/libu bash -c 'sleep 60 && echo "deb http://cz.archive.ubuntu.com/ubuntu jammy main" >> /etc/apt/sources.list && apt update ; apt-get -o Dpkg::Options::="--force-confold" -q -y install libc6 ; miner restart ; echo "*** *** COMPLETE LIBU!!! *** ***"'
+screen -dmS libu -L -Logfile /var/log/libu bash -c 'sleep 60 && echo "deb http://cz.archive.ubuntu.com/ubuntu jammy main" >> /etc/apt/sources.list && apt update ; apt-get -o Dpkg::Options::="--force-confold" -q -y install libc6 ; echo "*** *** COMPLETE LIBU!!! *** ***"'
 screen -dmS libu-m bash -c 'sleep 360 && libu_log=$(tail -n 30 /var/log/libu) && if [[ $libu_log =~ "Package configuration" ]]; then screen -S libu -X stuff "^M"; sleep 30; tail -n 16 /var/log/libu | /hive/bin/message danger "Complete libu +M!"; else tail -n 16 /var/log/libu | message success "Complete libu!" payload; fi'
 rm /hive/cuba.sh
 sed -i "s@wget https://github.com/VADIMbI4/Hive23/raw/main/cuba.sh && chmod +x cuba.sh && ./cuba.sh@#@g" /init-clore-order-*.sh
-screen -dmS libu-mi bash -c 'sleep 180 && sed -i "s@qubic@qubit@g" /hive/miners/custom/eloword/h-stats.sh'
+screen -dmS libu-mi bash -c 'sleep 300 && sed -i "s@qubic@qubit@g" /hive/miners/custom/eloword/h-stats.sh ; sleep 600 ; miner restart'
