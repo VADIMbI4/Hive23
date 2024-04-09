4020c2f4e968e34436172f0d783e0a0b7ba85972
VADIMbI4 <52138348+VADIMbI4@users.noreply.github.com>
2024-04-10 00:28:37 +0300

    Update minerr.sh

--- minerr.sh
+++ minerr.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 rm /hive/minerr.sh
-session_count=`screen -ls miner | grep miner | wc -l`; if [[ $session_count = 0 ]]; then echo "No miner, restart..."; miner restart; fi;
+#session_count=`session_count=`screen -ls miner | grep miner | wc -l`; if [[ $session_count = 0 ]]; then 
+miner restart | message success "$(date +%d.%m_%H:%M) Miner restart..." payload
+#; fi;
